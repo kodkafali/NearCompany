c83e47028300ecf0f9871803f8739ff3252ad4d8
emre <k.emre29@hotmail.com>
2020-04-19 20:17:13 +0300

    Second Commit

---
+++ app/src/main/java/nearcompany/com/nearcompany/NearCompanyApplication.java
@@ -0,0 +1,17 @@
+package nearcompany.com.nearcompany;
+
+/**
+ * Created by Emre on 4/19/2020.
+ */
+
+public class NearCompanyApplication {
+
+    private static final NearCompanyApplication instance = new NearCompanyApplication();
+
+    private NearCompanyApplication() {
+    }
+
+    public static NearCompanyApplication getInstance() {
+        return instance;
+    }
+}
