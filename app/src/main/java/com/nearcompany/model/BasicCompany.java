606a2f0cd868511a0cb05b363274d507c7b2e4dc
emre <k.emre29@hotmail.com>
2020-05-10 16:52:35 +0300

    [5] Added Admin Panel. Load data with 6 Teknoparks.

---
+++ app/src/main/java/com/nearcompany/model/BasicCompany.java
@@ -0,0 +1,18 @@
+package com.nearcompany.model;
+
+/**
+ * Created by Emre on 5/10/2020.
+ */
+
+public class BasicCompany {
+
+    public String name;
+    public String desc;
+    public String logo;
+
+    public BasicCompany(String name, String desc, String logo) {
+        this.name = name;
+        this.desc = desc;
+        this.logo = logo;
+    }
+}
